9f0ad01a7285c14e7ba91e307f60f3934a5969c2
MFaiqH <146482545+MFaiqH@users.noreply.github.com>
2023-09-30 04:01:15 +0800

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import Phaser from 'phaser'
+import GameOverScene from './scenes/GameOverScene'
+import MathFighterScene from './scenes/MathFighterScene'
+
+const config = {
+	type: Phaser.AUTO,
+	width: 480,
+	height: 640,
+	physics: {
+		default: 'arcade',
+		arcade: {
+			gravity: { y: 200 }
+		}
+	},
+	scale: {
+		mode: Phaser.Scale.FIT,
+		autoCenter: Phaser.Scale.CENTER_BOTH
+	},
+	scene: [MathFighterScene, GameOverScene]
+}
+
+export default new Phaser.Game(config)
